18c996507c70f407b5b2d67344957df06fc64b53
botsyrio <df1593@nyu.edu>
2020-12-08 20:26:59 -0500

    committing initial setup of the in-memory db and the album service

--- project2/src/app/services/InMemoryData/in-memory-data.service.ts
+++ project2/src/app/services/InMemoryData/in-memory-data.service.ts
@@ -1,3 +1,5 @@
+import { Image } from './../../models/image';
+import { User } from 'src/app/models/user';
 import { Album } from './../../models/album';
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
@@ -7,7 +9,22 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb(){
-    const albums: Album[] = [];
-    return albums;
+    let users: User[] = [
+      new User(1, "password", "test1@example.com"),
+      new User(2, "password2", "test2@example.com")
+    ]
+    let images: Image[] = [
+      new Image(1, 'https://www.petpaw.com.au/wp-content/uploads/2014/03/Basset-Hound-1.jpg', 'doggy',
+      new Date()),
+      new Image(2, 'https://4.bp.blogspot.com/-2bumVLpI6wE/ToOVU9_7OhI/AAAAAAAAAQY/6NathnDAGhk/s1600/bassethound.jpg',
+      'doggy 2', new Date())
+    ]
+    const albums: Album[] = [
+      new Album(1, 'bassets', users[0], images, 0, new Date(), [], [])
+    ];
+
+    console.log(albums[0])
+    console.log(albums[0].images)
+    return {albums};
   }
 }
--- project2/src/app/services/album/album.service.ts
+++ project2/src/app/services/album/album.service.ts
@@ -1,10 +1,20 @@
-import { HttpClient } from '@angular/common/http';
+import { Album } from './../../models/album';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   private albumsUrl = 'api/albums';
   
@@ -12,5 +22,32 @@ export class AlbumService {
     private http: HttpClient
   ) { }
 
+  getAlbums(): Observable<Album[]>{
+    return this.http.get<Album[]>(this.albumsUrl).pipe(
+      tap(_ => console.log('fetched albums')),
+      catchError(this.handleError<Album[]>('getAlbums', []))
+    );
+  }
 
+
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
