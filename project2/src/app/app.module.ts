2904294f43e655e6edada9cd9b1270c2f753a0cd
botsyrio <df1593@nyu.edu>
2020-12-08 22:01:51 -0500

    initial homepage made - displaying data

--- project2/src/app/app.module.ts
+++ project2/src/app/app.module.ts
@@ -10,13 +10,11 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/InMemoryData/in-memory-data.service';
 import { LoginComponent } from './login/login.component';
 import {FormsModule} from "@angular/forms";
-import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomepageComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
