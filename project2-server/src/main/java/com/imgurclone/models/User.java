f0a1f7620210a656c3a244e62b89a7dd5ef7fcc0
ratulahmed <ratulahmed1@gmail.com>
2020-12-09 14:41:54 -0500

    Created basic User entity class

--- project2-server/src/main/java/com/imgurclone/models/User.java
+++ project2-server/src/main/java/com/imgurclone/models/User.java
@@ -1,11 +1,43 @@
 package com.imgurclone.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    @Column(name = "email")
+    private String email;
+
+    //TODO make this a hashed valued
+    @Column(name = "hash")
+    private String hash;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
 }
