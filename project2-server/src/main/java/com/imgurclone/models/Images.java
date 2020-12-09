dee9ba861ea59798d438b9a2769e621a7b746e61
ratulahmed <ratulahmed1@gmail.com>
2020-12-09 14:39:43 -0500

    Created configurations and empty Models. PersistenceConfig still needs to be completed

---
+++ project2-server/src/main/java/com/imgurclone/models/Images.java
@@ -0,0 +1,4 @@
+package com.imgurclone.models;
+
+public class Images {
+}
---
+++ project2-server/src/main/java/com/imgurclone/models/User.java
@@ -0,0 +1,11 @@
+package com.imgurclone.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+
+}
