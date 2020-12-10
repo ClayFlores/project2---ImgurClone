ee50ae9dfacbf1b9cf4b0295c2b43bea3282eadc
ratulahmed <ratulahmed1@gmail.com>
2020-12-10 10:48:52 -0500

    User can be created - no validation or password hashing

--- project2-server/src/main/java/com/imgurclone/controllers/UserController.java
+++ project2-server/src/main/java/com/imgurclone/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.imgurclone.controllers;
 
+import com.imgurclone.daos.UserDao;
 import com.imgurclone.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users")
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserDao userDao;
 
     @GetMapping(path = "/")
     public ResponseEntity<User> getAllUsers() {
@@ -19,6 +24,7 @@ public class UserController {
     @PostMapping(path = "createUser")
     public ResponseEntity<?> createUser(@RequestBody User newUser) {
         System.out.println(newUser);
+        userDao.save(newUser);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
