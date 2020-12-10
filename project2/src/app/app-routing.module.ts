70b22735e2736b3843cb9276543143cf4dd19e33
ClayFlores <ClayFlores@users.noreply.github.com>
2020-12-10 15:58:50 -0800

    did a lot of the base work on edit album component and routed it and view album component too

--- project2/src/app/app-routing.module.ts
+++ project2/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import { AlbumViewComponent } from './album-view/album-view.component';
+import { AlbumEditComponent } from './album-edit/album-edit.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent },
-  {path:'', component: HomepageComponent}
+  {path:'', component: HomepageComponent},
+  {path:'album/:id', component: AlbumViewComponent},
+  {path:'album/:id/edit', component: AlbumEditComponent}
 ];
 
 @NgModule({
