18713cc73bc0d42e9f38a48d644c4c345627f401
ClayFlores <ClayFlores@users.noreply.github.com>
2020-12-10 12:57:50 -0800

    edited the album-view page slighty, contains date now

--- project2/src/app/album-edit/album-edit.component.ts
+++ project2/src/app/album-edit/album-edit.component.ts
@@ -10,6 +10,21 @@ import { User } from '../models/user';
 })
 export class AlbumEditComponent implements OnInit {
 
+  // sample album data 
+  users: User[] = [
+    new User(1, "pw", "test@test.com"),
+    new User(2, "pwpw", "a@b.com")
+  ]
+
+  imageSet: Image[] = [
+    new Image(1, '../assets/pizza.png','pizza', new Date()),
+    new Image(2, '../assets/macaron.png','macaron', new Date()),
+    new Image(3, '../assets/orange.png','orange', new Date()),
+    new Image(4, '../assets/broccoli.png', 'broccoli', new Date())
+  ]
+
+  Album = new Album(1, 'test images', this.users[0], this.imageSet, 0, new Date(), [],[])
+
     constructor() { }
 
     ngOnInit(): void {
