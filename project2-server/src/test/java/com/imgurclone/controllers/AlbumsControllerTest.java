04e26bcc705aa74d6b3bc628a020cc289c90b6ed
botsyrio <df1593@nyu.edu>
2020-12-15 15:08:20 -0500

    basic test running!!

---
+++ project2-server/src/test/java/com/imgurclone/controllers/AlbumsControllerTest.java
@@ -0,0 +1,44 @@
+package com.imgurclone.controllers;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:test-application-context.xml"})
+@WebAppConfiguration
+public class AlbumsControllerTest {
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void givenHomepageAlbumsURI_whenMockMVC_thenResponseOK() throws Exception {
+        MvcResult mvcResult = this.mockMvc
+                .perform(get("/albums/homepageAlbums"))
+                .andDo(print()).andExpect(status().isOk()).andReturn();
+        Assert.assertEquals("application/json;charset=UTF-8",mvcResult.getResponse().getContentType());
+    }
+}
