8f19407f6b723fadd2746fc0535274f4544d69e7
botsyrio <df1593@nyu.edu>
2020-12-16 14:07:07 -0500

    added createComment to AlbumsController.java, created CommentDao.java, and added @CreationTimestamp to Comment.java's date field

---
+++ project2-server/src/main/java/com/imgurclone/daos/CommentDao.java
@@ -0,0 +1,30 @@
+package com.imgurclone.daos;
+
+import com.imgurclone.models.Comment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class CommentDao {
+    private SessionFactory sessionFactory;
+
+    /**
+     * log4j logger
+     */
+    private static final Logger logger = LogManager.getLogger(AlbumDao.class);
+
+    @Autowired
+    public CommentDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public Comment insert(Comment comment){
+        Session session = sessionFactory.getCurrentSession();
+        session.save(comment);
+        return comment;
+    }
+}
