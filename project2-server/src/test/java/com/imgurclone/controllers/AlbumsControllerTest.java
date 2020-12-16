fa9b197b53d49379f0f3ec65490f7f8b2ac60de2
botsyrio <df1593@nyu.edu>
2020-12-15 19:24:29 -0500

    added givenNonExistentAlbumTag_whenMockMVC_thenResponseOK to AlbumsControllerTest.java

--- project2-server/src/test/java/com/imgurclone/controllers/AlbumsControllerTest.java
+++ project2-server/src/test/java/com/imgurclone/controllers/AlbumsControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.ArrayList;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -119,6 +120,23 @@ public class AlbumsControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].imageSet").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].tagList").exists())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.[*].commentSet").exists())
+                .andReturn();
+        Assert.assertEquals("application/json;charset=UTF-8",mvcResult.getResponse().getContentType());
+    }
+
+    //TODO: check that the albums have a tag with the correct name
+    /**
+     * tests that /albums/byTag/{tagName} returns an empty array of albums when given a tagName that doesn't exist
+     * @throws Exception
+     */
+    @Test
+    public void givenNonExistentAlbumTag_whenMockMVC_thenResponseOK() throws Exception {
+        String testTagName = "afgb7yawheufnaweuy8fbn9ufbhn";
+        MvcResult mvcResult = this.mockMvc
+                .perform(get("/albums/byTag/"+testTagName))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty())
                 .andReturn();
         Assert.assertEquals("application/json;charset=UTF-8",mvcResult.getResponse().getContentType());
     }
