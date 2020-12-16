d1ea0118f88ba5aa14b33185b91e2a7d1883871f
ClayFlores <40472619+ClayFlores@users.noreply.github.com>
2020-12-16 15:45:26 -0800

    added some collab stuff:

--- project2/src/app/album-edit/album-edit.component.ts
+++ project2/src/app/album-edit/album-edit.component.ts
@@ -5,6 +5,9 @@ import { Image } from '../models/image';
 import { User } from '../models/user';
 import { AlbumService } from '../services/album/album.service';
 import { FileUploadService } from '../services/fileUpload/file-upload.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-album-edit',
@@ -13,19 +16,50 @@ import { FileUploadService } from '../services/fileUpload/file-upload.service';
 })
 export class AlbumEditComponent implements OnInit {
 
-  album: Album;
+  album:   any;
   selectedIndex: number = -1;
 
-  fileToUpload: File |null = null;
+  //fileToUpload: File |null = null;
+  serverURL = "http://localhost:8080/files/upload";
+  //  uploadForm: FormGroup = new FormGroup(); 
+  
+//   handleFileInput(files: FileList) {
+//     this.fileToUpload = files.item(0);
+//     if (this.fileToUpload) {
+//       this.file.postFile(this.fileToUpload)
+//       .subscribe(uploaded => {
+//           console.log("came back " + uploaded)
+//       })
+//     }
+// }
 
-  handleFileInput(files: FileList) {
-    this.fileToUpload = files.item(0);
-    if (this.fileToUpload) {
-      this.file.postFile(this.fileToUpload)
-      .subscribe(uploaded => {
-          console.log("came back " + uploaded)
-      })
+onFileSelect(event: any) {
+  if (event.target.files.length > 0) {
+        const file = event.target.files[0];
+        //this.uploadForm.get('profile').setValue(file);
+
+        const formData = new FormData();
+        formData.append('file', file);
+        console.log(formData.get('file'));
+        const httpOptions = {
+          headers: new HttpHeaders({
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'multipart/form-data' 
+          })}
+        this.httpClient.post<any>(this.serverURL, formData, httpOptions ).subscribe(
+          (res) => console.log(res)
+    );
     }
+}
+
+onSubmit() {
+  const formData = new FormData();
+  //formData.append('file', this.uploadForm.get('profile').value);
+
+  this.httpClient.post<any>(this.serverURL, formData).subscribe(
+    (res) => console.log(res)
+  );
 }
 
 
@@ -50,15 +84,21 @@ public getAlbum() {
           });
       }
     }
+
     constructor(
       private albumService: AlbumService,
       private route: ActivatedRoute,
-      private file: FileUploadService
+      private file: FileUploadService,
+      private formBuilder: FormBuilder,
+      private httpClient: HttpClient
     ) { }
 
     ngOnInit(): void {
 
         this.getAlbum();
+        // this.uploadForm = this.formBuilder.group({
+        //   profile: ['']
+        //});
     }
   
 }
--- project2/src/app/services/fileUpload/file-upload.service.ts
+++ project2/src/app/services/fileUpload/file-upload.service.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { createConnection } from 'net';
+//import { createConnection } from 'net';
 
 
 @Injectable({
@@ -12,34 +12,34 @@ import { createConnection } from 'net';
 })
 export class FileUploadService {
 
-  postFile(fileToUpload: File): Observable<boolean> {
-    const endpoint = 'localhost:8080/project2-server/files/upload';
-    const formData: FormData = new FormData();
-    const headers = new HttpHeaders ({
-      'Content-Type': 'application/x-www-for-urlencoded'
-    });
-    const imageCaption: string | null = "";
-    const albumId: number = 1;
-    formData.append('user-file', fileToUpload, imageCaption); // filekey to user-file, 3rd arg from fileToUpload.name
-    return this.httpClient
-      .post(endpoint, formData, {headers})
-        .pipe(map (data => {return true}),
-          catchError((e) => this.handleError(e))
-        );
-}
+  // postFile(fileToUpload: File): Observable<boolean> {
+  //   const endpoint = 'localhost:8080/project2-server/files/upload';
+  //   const formData: FormData = new FormData();
+  //   const headers = new HttpHeaders ({
+  //     'Content-Type': 'application/x-www-for-urlencoded'
+  //   });
+  //   const imageCaption: string | null = "";
+  //   const albumId: number = 1;
+  //   formData.append('user-file', fileToUpload, imageCaption); // filekey to user-file, 3rd arg from fileToUpload.name
+  //   return this.httpClient
+  //     .post(endpoint, formData, {headers})
+  //       .pipe(map (data => {return true}),
+  //         catchError((e) => this.handleError(e))
+  //       );
+//}
 
-private handleError<T>(operation = 'operation', result?: T) {
-  return (error: any): Observable<T> => {
+// private handleError<T>(operation = 'operation', result?: T) {
+//   return (error: any): Observable<T> => {
 
-    // TODO: send the error to remote logging infrastructure
-    console.error(error); // log to console instead
+//     // TODO: send the error to remote logging infrastructure
+//     console.error(error); // log to console instead
 
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
+//     // TODO: better job of transforming error for user consumption
+//     console.log(`${operation} failed: ${error.message}`);
 
-    // Let the app keep running by returning an empty result.
-    return of(result as T);
-  };
-}
+//     // Let the app keep running by returning an empty result.
+//     return of(result as T);
+//   };
+// }
   constructor(public httpClient:HttpClient) { }
 }
