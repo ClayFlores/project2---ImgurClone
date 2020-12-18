22e9352cf627f46606e632751e00ad2be521eb25
botsyrio <df1593@nyu.edu>
2020-12-17 19:55:18 -0500

    added jsDocs to AlbumEditGuard

--- project2/src/app/guards/albumEdit/album-edit.guard.ts
+++ project2/src/app/guards/albumEdit/album-edit.guard.ts
@@ -6,10 +6,23 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * route guard that determines whether the user can access the given album edit page
+ */
 export class AlbumEditGuard implements CanActivate {
 
+  /**
+   * constructor for the AlbumEditGuard
+   * @param albumService - album service - used to communicate with the server
+   */
   constructor(private albumService: AlbumService){}
 
+  /**
+   * determines whether the user can activate the current route -- lets them through if there is a valid current user AND
+   * that user created the album in question
+   * @param route - the the user is trying to access
+   * @param state - the state of the router
+   */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
