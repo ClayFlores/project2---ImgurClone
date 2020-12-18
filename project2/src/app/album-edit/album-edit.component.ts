2058ec6302562ca7a95f34e1a93f4c702ec3af70
botsyrio <df1593@nyu.edu>
2020-12-17 19:34:50 -0500

    added jsDocs to AlbumEditComponent

--- project2/src/app/album-edit/album-edit.component.ts
+++ project2/src/app/album-edit/album-edit.component.ts
@@ -18,23 +18,47 @@ import {Router} from '@angular/router';
   templateUrl: './album-edit.component.html',
   styleUrls: ['./album-edit.component.css']
 })
+/**
+ * component for album editing
+ */
 export class AlbumEditComponent implements OnInit {
 
+  /**
+   * the album being edited
+   */
   album: any;
   selectedIndex: number = -1;
+  /**
+   * id of the album to be deleted
+   */
   idToDelete: number = -1;
 
+  /**
+   * represents the data in the image upload form
+   */
   form: FormData;
+  /**
+   * represents the caption in the image upload form
+   */
   imageCaption = '';
+  /**
+   * represents the name of the tag to be uploaded in the new tag form
+   */
   newTag = '';
 
   // fileToUpload: File |null = null;
+  /**
+   * the url for image uploading
+   */
   serverURL = 'http://localhost:8080/project2-server/files/upload';
   //  uploadForm: FormGroup = new FormGroup();
 
 
 
-
+/**
+ * runs when a file is selected to be uploaded. appends the name of the selected album to the form
+ * @param event - the file selection event
+ */
 onFileSelect(event: any) {
   if (event.target.files.length > 0) {
         const file = event.target.files[0];
@@ -48,6 +72,10 @@ onFileSelect(event: any) {
     this.getAlbum(); // called after to hopefully show the new addition
 }
 
+/**
+ * deletes image selected in the image deletion form from the album and the database
+ * @param _idToDelete - the id of the image to be deleted from the server
+ */
 onDeleteSubmit(_idToDelete: number){
   if (_idToDelete && _idToDelete != -1) {
       this.albumService.deleteImageFromAlbum(_idToDelete)
@@ -55,7 +83,9 @@ onDeleteSubmit(_idToDelete: number){
         this.getAlbum(); // should refresh the album with one less image
   }
 }
-
+/**
+ * runs when the image upload form is submitted - sends a post request to the server to upload the image
+ */
 onSubmit() {
   const url = 'album/' + this.album.id + '/edit';
   this.form.append('imageCaption', this.imageCaption);
@@ -76,8 +106,9 @@ public setRow(_index: number) {
 
 
 
-// this is not a great strategy, repeating code from album-view.ts
-// ideally, would send the album over with the routing
+/**
+ * retrieves the album to be edited from the server and organizes it to fit our album model
+ */
 public getAlbum() {
       if (this.route.snapshot.paramMap.get('id')) { // did not like the possibility of id being null, this condition verifies it isnt
         const id =  this.route.snapshot.paramMap.get('id');
@@ -122,6 +153,16 @@ public getAlbum() {
             });
       }
     }
+    
+    /**
+     * constructor for the AlbumEdit component
+     * @param albumService - album service - used to communicate with the server for everything except image creation
+     * @param route - the route for the current page
+     * @param file 
+     * @param formBuilder 
+     * @param httpClient - the httpClient - used to upload the image to the server
+     * @param router - router object - used to redirect the user
+     */
     constructor(
       private albumService: AlbumService,
       private route: ActivatedRoute,
@@ -139,7 +180,9 @@ public getAlbum() {
         console.log('inside ngoninit' , this.album);
     }
 
-    // TODO need some kind of alert that it was successfully created
+  /**
+   * submits the new tag in the tag form to the server if the input is valid - otherwise returns
+   */
   tagSubmit() {
     const url = 'album/' + this.album.id + '/edit';
     if (this.newTag === '') {
