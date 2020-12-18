1b3e904b2edba0f84d6ddbdbccf3d38d4db18373
botsyrio <df1593@nyu.edu>
2020-12-17 21:37:06 -0500

    added jsDocs to RegistrationComponent

--- project2/src/app/registration/registration.component.ts
+++ project2/src/app/registration/registration.component.ts
@@ -7,16 +7,33 @@ import {HttpClient} from '@angular/common/http';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
+/**
+ * view for registering a new user
+ */
 export class RegistrationComponent implements OnInit {
 
+  /**
+   * the contents of the email field on the form
+   */
    email: string = '';
+   /**
+    * the contents of the password field on the form
+    */
    password: string = '';
 
+   /**
+    * Constructor for the RegistrationComponent
+    * @param router the router object - used to redirect the user on successful registration
+    * @param http the httpClient - used to communicate with the server
+    */
   constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
   }
 
+  /**
+   * attempts to register the user with the given credentials
+   */
   onSubmit() {
     const request = {
       email: this.email,
