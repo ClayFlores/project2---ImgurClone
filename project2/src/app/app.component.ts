96d45dd89e456efe459185d9d600c79c23b4e771
botsyrio <df1593@nyu.edu>
2020-12-17 22:00:31 -0500

    added jsDocs to AppComponent

--- project2/src/app/app.component.ts
+++ project2/src/app/app.component.ts
@@ -5,18 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/**
+ * root component of the application
+ */
 export class AppComponent {
+  /**
+   * title of the application
+   */
   title = 'project2';
+  /**
+   * string representation of the id of the currently logged-in user
+   */
   userId:string|null = null;
 
+  /**
+   * runs when root component is initialized -- gets user id from local storage
+   */
   ngOnInit(){
    this.userId = localStorage.getItem('userId')
   }
 
+  /**
+   * logs in user -- responds to login event
+   */
   loginResponse():void{
     console.log('app component caught onLogin')
     this.userId = localStorage.getItem('userId')
   }
+  /**
+   * logs out user -- responds to logout event
+   */
   logoutResponse():void{
     console.log('app component caught onLogout');
     this.userId = localStorage.getItem('userId');
