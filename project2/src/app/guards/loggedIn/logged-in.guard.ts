3ca45ab9a7045a8d18f547b9d45b00282fe53de2
botsyrio <df1593@nyu.edu>
2020-12-17 19:57:09 -0500

    added jsDocs to LoggedInGuard

--- project2/src/app/guards/loggedIn/logged-in.guard.ts
+++ project2/src/app/guards/loggedIn/logged-in.guard.ts
@@ -5,7 +5,16 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * route guard used to block users out of pages if they haven't been logged-in
+ */
 export class LoggedInGuard implements CanActivate {
+  /**
+   * returns true if the user can activate the route, false otherwise.
+   * will return true if the user is validly logged in
+   * @param route - route the user is trying to activate
+   * @param state - the state of the router
+   */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
