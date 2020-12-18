b7ec4f4c1de585e792895a067ecb3c69601c4f88
botsyrio <df1593@nyu.edu>
2020-12-17 20:38:20 -0500

    added jsDocs to LikeComponent

--- project2/src/app/like/like.component.ts
+++ project2/src/app/like/like.component.ts
@@ -6,18 +6,42 @@ import { AlbumService } from '../services/album/album.service';
   templateUrl: './like.component.html',
   styleUrls: ['./like.component.css']
 })
+/**
+ * component for displaying an album's likes and allowing a signed-in user to like the album
+ */
 export class LikeComponent implements OnInit {
 
   
+  /**
+   * the id of the album whose like-logic is being handled
+   */
   @Input() albumId:number=-1;
+  /**
+   * the id of the currently signed-in user
+   */
   myUserId:string|null = null;
+  /**
+   * the number of likes the current logic has
+   */
   numLikes:number=0;
+  /**
+   * boolean representation of whether the currently signed-in user has liked the album
+   */
   isLiked:boolean=false;
 
+  /**
+   * Constructor for LikeComponent
+   * @param albumService - album service - used to communicate with the server
+   */
   constructor(
     private albumService: AlbumService
   ) { }
 
+  /**
+   * runs as soon as the LikeComponent has been initialized 
+   * -- sets the currently signed-in user, retrieves the number of likes if the album id is set properly, 
+   * and checks whether the user has liked the album if they're signed in
+   */
   ngOnInit(): void {
     this.myUserId = localStorage.getItem('userId');
     if(this.albumId !== -1){
@@ -28,6 +52,11 @@ export class LikeComponent implements OnInit {
     }
   }
 
+  /**
+   * runs as soon as an input field has been changed 
+   * -- sets the currently signed-in user, retrieves the number of likes if the album id is set properly, 
+   * and checks whether the user has liked the album if they're signed in
+   */
   ngOnChanges(){
     if(this.albumId !== -1){
       this.getNumLikes();
@@ -37,6 +66,9 @@ export class LikeComponent implements OnInit {
     }
   }
 
+  /**
+   * retrieves the number of total likes for the current albm
+   */
   getNumLikes():void{
     this.albumService.getNumLikes(this.albumId)
       .subscribe(result=>{
@@ -44,6 +76,9 @@ export class LikeComponent implements OnInit {
       });
   }
 
+  /**
+   * checks whether the currently signed-in user has liked the album
+   */
   checkIsAlbumInLikes(): void{
     if(this.myUserId)
       this.albumService.getIsAlbumInMyLikes(+this.myUserId, this.albumId)
@@ -54,6 +89,9 @@ export class LikeComponent implements OnInit {
       });
   }
 
+  /**
+   * the currently signed-in user likes the album
+   */
   likeAlbum():void{
     if(!this.isLiked && this.myUserId){
       this.numLikes++;
